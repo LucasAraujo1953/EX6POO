5bbcd6ff090079d4c5b86bd09d08078254ee346e
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-22 18:49:31 -0300

    Add files via upload

---
+++ EX6POO/Conversor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX6POO
+{
+    internal class Conversor
+    {
+        private double cotacaoDolar;
+
+        public Conversor(double cotacao)
+        {
+            cotacaoDolar = cotacao;
+        }
+
+        public double ConverterParaReais(double valorDolares)
+        {
+            return valorDolares * cotacaoDolar;
+        }
+    }
+}
+
---
+++ EX6POO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX6POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cotação do Dólar: ");
+            double cotacao = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de dólares: ");
+            double quantidadeDolares = Convert.ToDouble(Console.ReadLine());
+
+            Conversor conversor = new Conversor(cotacao);
+            double valorEmReais = conversor.ConverterParaReais(quantidadeDolares);
+
+            Console.WriteLine($"O valor em Reais é: R$ {valorEmReais}");
+        }
+    }
+}
